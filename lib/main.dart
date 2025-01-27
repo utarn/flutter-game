a48465fa0485b24146e47dd36cffdbf53e413c9d
Utarn Buranasaksee <utarn@yahoo.com>
2025-01-27 09:11:10 +0700

    Change to new name as suggested

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo 555 + 555',
+      title: 'Flutter Demo 555 + 555 New name',
       theme: ThemeData(
         // This is the theme of your application.
         //
